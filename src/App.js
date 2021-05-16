c2ec3a56dcbd7d3ff60302338ce71fa17770eb11
Akshay Adme <akshayadme@gmail.com>
2021-05-16 09:19:29 +0530

    created private & public route

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import React from "react";
-import "rsuite/dist/styles/rsuite-default.css";
-import "./styles/main.scss";
+import React from 'react';
+import { Switch } from 'react-router';
+import 'rsuite/dist/styles/rsuite-default.css';
+import Home from './components/pages/Home';
+import SignIn from './components/pages/SignIn';
+import PrivateRoute from './components/PrivateRoute';
+import PublicRoute from './components/PublicRoute';
+import './styles/main.scss';
 
 function App() {
-  return <div>hello</div>;
+  return (
+    <Switch>
+      <PublicRoute exact path="/signin">
+        <SignIn />
+      </PublicRoute>
+      <PrivateRoute exact path="/">
+        <Home />
+      </PrivateRoute>
+    </Switch>
+  );
 }
 
 export default App;
--- src/misc/firebase.js
+++ src/misc/firebase.js
@@ -1,13 +1,13 @@
-import firebase from "firebase/app";
+import firebase from 'firebase';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAtMLfW-MzL-cmCJDjfQ8stK0J9suTMp94",
-  authDomain: "chat-web-app-50955.firebaseapp.com",
-  projectId: "chat-web-app-50955",
-  storageBucket: "chat-web-app-50955.appspot.com",
-  messagingSenderId: "37683637791",
-  appId: "1:37683637791:web:fbd318ea0667f39799ef2a",
+  apiKey: 'AIzaSyAtMLfW-MzL-cmCJDjfQ8stK0J9suTMp94',
+  authDomain: 'chat-web-app-50955.firebaseapp.com',
+  projectId: 'chat-web-app-50955',
+  storageBucket: 'chat-web-app-50955.appspot.com',
+  messagingSenderId: '37683637791',
+  appId: '1:37683637791:web:fbd318ea0667f39799ef2a',
 };
 
 // Initialize Firebase
